78bb93f6f45332ac33743e1cf780c68cf4bc8ab3
gemnets <154671337+gemnets@users.noreply.github.com>
2024-10-08 07:52:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/e-plantShopping/', // Ensure this matches your GitHub repository name
+  base: '/1e-plantShopping/', // Ensure this matches your GitHub repository name
   plugins: [react()],
 });
